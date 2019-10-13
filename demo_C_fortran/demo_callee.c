8bfb253da6d5da6aff8903f5e46b97a3304c7b56
root <root@pcdell-de-fred.tournesol59>
2019-10-13 17:14:32 +0200

    Last version of mixed C Fortran try programming

--- demo_C_fortran/demo_callee.c
+++ demo_C_fortran/demo_callee.c
@@ -1,4 +1,4 @@
-// Simple test mixed C-fortran
+// Simple test mixed C-fortran, only for scalar or simple arrays
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -11,19 +11,40 @@ typedef struct TABLE {
         double* x;
 } *tTABLE;
 
-void democallee_(void * forfortran) {
+extern struct
+{
+  int ii,jj,kk;
+} ijk_;
+
+void democallee(double * forfortran) {
 
   int i,j; // n,l
   tINFO myinfo = malloc(sizeof(struct INFO));
+  double myscalar=10.0;
 
   printf("just after calling C\n");
   
  // double mydouble[10] = {3.14, 2.21, 1.72, 1.41, 1., 1.57, 1.10, 0.788, 0.707, 0.5};
 
   for (i=0; i<1; i++) {
-     myinfo->x[i]=i+1.5;
+     myinfo->x[i]=((float) i)+1.5;
 
   }
-  printf("after call before typecasting and return to fortran\n");
-  forfortran = (void*) myinfo;
+  printf("after call before return to fortran\n");
+ // forfortran = (void*) myptr; // type casting do not work for me
+  myscalar=forfortran[0];
+  printf("before return to fortran, value copied in C: %f\n", myscalar);
+}
+
+
+int doubleijk_(char *cc, int ll)
+{
+
+  cc[ll--]='\0';  // NULL terminate the string
+  printf("From doubleIJK: %s \n",cc);
+  ijk_.ii *=2;
+  ijk_.jj *=2;
+  ijk_.kk *=2;
+
+  return (1);
 }
--- demo_C_fortran/main.f90
+++ demo_C_fortran/main.f90
@@ -6,16 +6,21 @@ use ISO_C_BINDING
 implicit none
 
    type(c_ptr)        :: pc
-  ! real, dimension(10), allocatable  :: tfd(:)
-   real,pointer       :: tfd(:)
-   real,pointer                        :: pf1(:)
+!!   real*8, dimension(10), allocatable  :: tfd(:)
+   real*8,pointer       :: tfd(:)
+  ! real,pointer                        :: pf1(:)
    integer n,l,i,j,allocstat
+  integer ii,jj,kk
+   common/ijk/ ii, jj, kk
+   real*8 ff
+   character*32 cc
 
    interface
-      subroutine DEMO_CALLEE(pt3) bind(C, name="democallee")
+      subroutine DEMO_CALLEE(d3) bind(C, name="democallee")
          use ISO_C_BINDING, only : c_double
          implicit none
-         real(c_double),intent(inout)  :: pt3
+         real(c_double),intent(inout)          :: d3
+!         real(c_double),intent(inout),pointer  :: pt3(:)
       end subroutine DEMO_CALLEE
    end interface
 
@@ -25,10 +30,40 @@ implicit none
    if (allocstat.ne.0) then
      print *, 'Error not all fields allocated!'
    end if
-   
-   call DEMOCALLEE(tfd)
-  
-   write(*,103) "value transmitted from C: ", tfd(1)
-103 format(a,f10.5)  
+   do i=1,n
+     tfd(i)=1.5
+   enddo
+   write(*,103) "value copied to C: ", tfd(1)
 
-end program demo_callC
+   call DEMO_CALLEE(tfd(1))
+
+
+   ii=2
+   jj=3
+   kk=4
+   ff=9.0567
+   cc='Example of a character string'
+   
+   write(6,100) ii,ff
+   call abc(ii)
+   write(6,200) ii
+   write(6,300) ii,jj,kk
+
+   call doubleIJK(cc)
+   write(6,300) ii,jj,kk
+   write(6,400) cc
+
+   stop 
+  
+103 format(a,f10.5)  
+100 format('ii= ',i2,' ff= ', f10.4)
+200 format('ii ', i2)
+300 format('ii= ',i2, ' jj= ',i2,' k= ', i2)
+400 format(a32)
+
+end !program demo_callC
+
+subroutine abc(jj)
+  jj=jj*2
+  return
+end !subroutine abc
